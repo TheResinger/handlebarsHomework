f2d89e5c46d009d7b1a4ca3c11b1f1f83b15f30b
Benjamin Morrow <morrowbenjamin42@gmail.com>
2019-09-21 00:21:45 -0400

    change db stuff to env file

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: "us-cdbr-iron-east-02.cleardb.net",// process.env.MYSQLHOST,
+    host: process.env.MYSQLHOST,
     port: 3306,
-    user: "b93bc7d520e870",//process.env.MYSQLUSER,
-    password: "556103f7",//process.env.MYSQLPASS,
-    database: "heroku_2505dd4dad9d6cb"//process.env.MYSQLDATABASE
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASS,
+    database: process.env.MYSQLDATABASE
 });
 
 connection.connect((err) => {
