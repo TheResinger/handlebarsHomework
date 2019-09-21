94a98ad104d4b4ada906a4a08f31f43dfcacc804
Benjamin Morrow <morrowbenjamin42@gmail.com>
2019-09-21 00:20:38 -0400

    Heroku login push

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: "localhost",// process.env.MYSQLHOST,
+    host: "us-cdbr-iron-east-02.cleardb.net",// process.env.MYSQLHOST,
     port: 3306,
-    user: "root",//process.env.MYSQLUSER,
-    password: "root",//process.env.MYSQLPASS,
-    database: "burgers_db"//process.env.MYSQLDATABASE
+    user: "b93bc7d520e870",//process.env.MYSQLUSER,
+    password: "556103f7",//process.env.MYSQLPASS,
+    database: "heroku_2505dd4dad9d6cb"//process.env.MYSQLDATABASE
 });
 
 connection.connect((err) => {
